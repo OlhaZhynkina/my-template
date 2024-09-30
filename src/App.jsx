fa7533fe8ed4e53122a8321726156e7cca324f3c
Olha <olyasavvz@gmail.com>
2024-09-30 18:38:55 +0300

    done template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Container from "./components/Container/Container";
+import Section from "./components/Section/Section";
+
+function App() {
+  return (
+    <Section>
+      <Container></Container>
+    </Section>
+  );
+}
+
+export default App;
